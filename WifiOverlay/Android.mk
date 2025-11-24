f097f6bb109d3659407e17ba2640e2591e483e97
baimeeb <74864183+baimeeb@users.noreply.github.com>
2025-11-24 17:08:15 +0800

    silm packages

--- WifiOverlay/Android.mk
+++ WifiOverlay/Android.mk
@@ -5,4 +5,20 @@ LOCAL_PACKAGE_NAME := treble-overlay-wifi
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_OVERRIDES_PACKAGES := \
+    Updater \
+    ExactCalculator \
+    Email \
+    Exchange2 \
+    com.android.emailcommon \
+    Calendar \
+    CarrierDefaultApp \
+    SimAppDialog \
+    WAPPushManager \
+    SecureElement \
+    BlockedNumberProvider \
+    CallLogBackup \
+    messaging
+
 include $(BUILD_PACKAGE)
--- overlay.mk
+++ overlay.mk
@@ -1,18 +1,3 @@
 PRODUCT_PACKAGES += \
-	HardwareOverlayPicker \
-	QtiAudio \
-	TrebleApp \
-	treble-overlay-NavBar \
-	treble-overlay-NightMode \
 	treble-overlay-SystemUI-FalseLocks \
-	treble-overlay-devinputjack \
-	treble-overlay-highpriomisc \
-	treble-overlay-misc-aod \
-	treble-overlay-misc-aod-systemui \
-	treble-overlay-misc-dt2w \
-	treble-overlay-misc-minimal-brightness \
-	treble-overlay-misc-spen-pointer \
-	treble-overlay-misc-transparent-pointer \
-	treble-overlay-tethering \
-	treble-overlay-tethering-nobpf \
 	treble-overlay-wifi \
